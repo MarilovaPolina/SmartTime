b33271058ca9b779084362178ed308aadb271669
MarilovaPolina <64793906+MarilovaPolina@users.noreply.github.com>
2023-11-29 23:18:48 +0400

    до вырубки ненужного
    до вырубки ненужного

--- index.php
+++ index.php
@@ -24,42 +24,11 @@
 <body>
 
 <?php include("app/include/header.php"); ?>
-
-<!-- блок карусели START-->
+ 
 <div class="container">
-    <div class="row">
-        <h2 class="slider-title">Топ публикации</h2>
-    </div>
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <?php foreach ($topTopic as $key => $post): ?>
-                <?php if($key == 0):?>
-                    <div class="carousel-item active">
-                <?php else: ?>
-                    <div class="carousel-item">
-                <?php endif; ?>
-                        <img src="<?=BASE_URL . 'assets/images/posts/' . $post['img'] ?>" alt="<?=$post['title']?>" class="d-block w-100">
-                        <div class="carousel-caption-hack carousel-caption d-none d-md-block">
-                            <h5><a href="<?=BASE_URL . 'single.php?post=' . $post['id'];?>"><?=substr($post['title'], 0, 120) . '...'  ?></a></h5>
-                        </div>
-                    </div>
-            <?php endforeach; ?>
-
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-</div>
-<div class="container">
-    <div class="content row">
-        <div class="main-content col-md-9 col-12">
-            <h2>Последние публикации</h2>
+    <div class="content">
+        <div class="main-content col-12">
+            <h2>Наши товары</h2>
             <?php foreach ($posts as $post): ?>
                 <div class="post row">
                     <div class="img col-12 col-md-4">
@@ -78,51 +47,13 @@
                     </div>
                 </div>
             <?php endforeach; ?>
-            <?php include("app/include/pagination.php"); ?>
         </div>
-        <!-- sidebar Content -->
-        <div class="sidebar col-md-3 col-12">
-
-            <div class="section search">
-                <h3>Поиск</h3>
-                <form action="search.php" method="post">
-                    <input type="text" name="search-term" class="text-input" placeholder="Введите искомое слово...">
-                </form>
-            </div>
-
-
-            <div class="section topics">
-                <h3>Категории</h3>
-                <ul>
-                    <?php foreach ($topics as $key => $topic): ?>
-                    <li>
-                        <a href="<?=BASE_URL . 'category.php?id=' . $topic['id']; ?>"><?=$topic['name']; ?></a>
-                    </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-
-        </div>
-
     </div>
 
 </div>
 
-<!-- блок main END-->
-<!-- footer -->
 <?php include("app/include/footer.php"); ?>
-<!-- // footer -->
 
-
-<!-- Optional JavaScript; choose one of the two! -->
-
-<!-- Option 1: Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
--->
 </body>
 </html>
